6ae9f3234e4532dae45055751b3f85983f98fcee
Ajith <130132590+ajith2189@users.noreply.github.com>
2025-05-27 12:22:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const session = require("express-session");
 const nocache = require("nocache");
 
-// Middle  dwar e to handle sessions
+// Middle  dware to handle sessions
 app.use(
   session({
     secret: "your_secret_key",
